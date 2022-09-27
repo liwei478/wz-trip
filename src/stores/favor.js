ff9c9920a62f3aeec8fb518cffdab77eb3604002
lwz <1055824818@qq.com>
2022-09-27 22:13:49 +0800

    add favor

---
+++ src/stores/favor.js
@@ -0,0 +1,21 @@
+import { defineStore } from "pinia"
+import { getFavorList, getHistoryList } from "@/services/modules/favor"
+
+const useFavorStore = defineStore("favor", {
+  state: () => ({
+    favorList: [],
+    historyList: []
+  }),
+  actions: {
+    async fetchFavorList() {
+      const res = await getFavorList()
+      this.favorList = res.data.data.items
+    },
+    async fetchHistoryList() {
+      const res = await getHistoryList()
+      this.historyList = res.data.data.items
+    }
+  }
+})
+
+export default useFavorStore
